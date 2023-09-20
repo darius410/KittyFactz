60c65fba436d4a0c17ddc2a6d7ff7a2cc3cf9b63
Chaos <tinywonk@yahoo.com>
2023-09-20 17:07:41 -0400

    changed channel name to Kitties Please

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const twitchApi = require("tmi.js");
 const axios = require('axios').default;
 const API_KEY = process.env.API_KEY
 const API_SECRET = process.env.API_SECRET
+const OAUTH_TOKEN = process.env.OAUTH_TOKEN
+const CLIENT_ID = process.env.CLIENT_ID
 
 
 const config = {
@@ -20,36 +22,46 @@ const config = {
     },
     channels:[
         "gameswithchaos" ,
-       "showmeurkittiesplz"
+        "kittyfactzplz"
 ],
         API_KEY:API_KEY ,
         API_SECRET:API_SECRET
     
 };
+
+
+// https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=4tlii5iepld5qemo3853nq28grxczd&redirect_uri=http%3A%2F%2Flocalhost&scope=channel%3Amanage%3Aredemptions+channel%3Aread%3Aredemptions+channel%3Aread%3Asubscriptions+moderator%3Aread%3Achatters+channel%3Aread%3Ahype_train+bits%3Aread
+// 4tlii5iepld5qemo3853nq28grxczd
+
+
+
 const chatBot = new twitchApi.client(config);
 
 
-async function isChannelLive(channelName) {
+async function isChannelLive(channel) {
     return axios
-      .get(`https://api.twitch.tv/helix/streams?user_login=${channelName}`, {
-        headers: {
-          "Client-ID": API_KEY
+      .get(`https://api.twitch.tv/helix/streams?user_login=kittyfactzplz`, {
+        headers: {              
+          'Authorization': `Bearer ${OAUTH_TOKEN}`,
+          'Client-ID':CLIENT_ID
         }
       })
       .then(response => {
-        return response.data.data.length > 0; // If there's data in the response, the channel is live
+        
+        return response.data.data.length > 0;
+        // If there's data in the response, the channel is live
       })
-      .catch(error => {
-        console.error("Error checking stream status:", error);
-        return false; // Default to not live if there's an error
-      });
+      // .catch(error => {
+      //   console.error("Error checking stream status:", error);
+      //   return false; // Default to not live if there's an error
+      // });
   }
 
 async function kittyFactz (channel,userstate,message,self){
     const isLive = await isChannelLive(channel);
     if (typeof(message) =='string'){
         const userMessage = message.split(' ');
-    }
+    
     
 if(isLive){
     if(/^!kittyfact$/i.test(userMessage[0])){
@@ -75,6 +87,7 @@ if(isLive){
         })
     };
     }
+  }
 }
 
 
@@ -94,7 +107,7 @@ setInterval(async() => {
 
 chatBot.on('message', (channel, userstate, message, self) => {
     if (/^!kittyfact$/i.test(message)) {
-      kittyFactz(channel);
+      kittyFactz(channel,userstate);
     }
   });
 // chatBot.on('message', kittyFactz)
